26d59d348a221aae21c2c5bb1e8527d1ef150c5b
Hishaam NAMOOYA <hishaam.namooya@versusmind.eu>
2024-05-25 23:01:18 +0400

    Initial Commit

---
+++ Managers/PackageSinkManager.cs
@@ -0,0 +1,45 @@
+﻿namespace ArcwaveConsole.PackageReader.Managers
+{
+    using Sitecore.Globalization;
+    using Sitecore.Install;
+    using Sitecore.Install.Framework;
+    using Sitecore.Install.Utils;
+    using System;
+    using System.Text;
+
+    public class PackageSinkManager : BaseSink<PackageEntry>
+    {
+        public override void Put(PackageEntry entry)
+        {
+            Console.WriteLine($"Key: {entry.Key}. Source: {PackageUtils.TryGetValue(entry.Properties, "source")}.");
+        }
+
+        private string FormatInstallOptions(BehaviourOptions options)
+        {
+            if (!options.IsDefined)
+            {
+                return Translate.Text("Undefined");
+            }
+                
+            InstallMode itemMode = options.ItemMode;
+
+            if (itemMode == InstallMode.Undefined)
+            {
+                return Translate.Text("Ask User");
+            }
+
+            var stringBuilder = new StringBuilder(50);
+            
+            stringBuilder.Append(Translate.Text(itemMode.ToString()));
+            
+            if (itemMode == InstallMode.Merge)
+            {
+                stringBuilder.Append(" / ");
+
+                stringBuilder.Append(Translate.Text(options.ItemMergeMode.ToString()));
+            }
+            
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArcwaveConsole.PackageReader
+{
+    using Sitecore.Install.Framework;
+    using Sitecore.Install.Utils;
+    using System;
+    using ArcwaveConsole.PackageReader.Managers;
+    using Sitecore.Install;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var filePath = $@"Update filepath of the Package.zip";
+
+            ISource<PackageEntry> source = new Sitecore.Install.Zip.PackageReader(filePath);
+
+            var packageSinkHelper = new PackageSinkManager();
+
+            packageSinkHelper.Initialize(Installer.CreatePreviewContext());
+            
+            new EntrySorter(source).Populate(packageSinkHelper);
+
+            Console.ReadKey();
+
+        }
+    }
+}
